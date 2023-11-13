f5b197db92a40a3b73bf2a796bef3b83729f8803
Björn Unnar <35260726+bjornunnar@users.noreply.github.com>
2023-11-13 20:47:42 +0000

    start new expense addition

--- lib/widgets/expenses_base.dart
+++ lib/widgets/expenses_base.dart
@@ -3,49 +3,61 @@ import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
-class Expenses extends StatefulWidget{
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
   @override
-  State<Expenses> createState(){
+  State<Expenses> createState() {
     return _ExpensesState();
   }
 }
 
-class _ExpensesState extends State<Expenses>{
-
-  void _openAddExpenseOverlay(){
+class _ExpensesState extends State<Expenses> {
+  void _openAddExpenseOverlay() {
     showModalBottomSheet(
-      context: context, 
-      builder: (ctx) => const NewExpense());
+        context: context, builder: (ctx) => NewExpense(addNewExpense));
   }
 
   // create dummy data
   final List<Expense> _registeredExpenses = [
-    Expense(title: "Flutter Course", amount: 19.99, date: DateTime.now(), category: Category.work),
-    Expense(title: "Groceries", amount: 5.20, date: DateTime.now(), category: Category.food),
-    Expense(title: "Bus Fare", amount: 2.90, date: DateTime.now(), category: Category.travel),
+    Expense(
+        title: "Flutter Course",
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: "Groceries",
+        amount: 5.20,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: "Bus Fare",
+        amount: 2.90,
+        date: DateTime.now(),
+        category: Category.travel),
   ];
+
+  void addNewExpense(Expense data) {
+    setState(() {
+      _registeredExpenses.add(data);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Expense Tracker"),
-        actions: [
-          IconButton(
-            onPressed: _openAddExpenseOverlay,
-            icon: const Icon(Icons.add),
-            )
-        ]
-      ),
+      appBar: AppBar(title: const Text("Expense Tracker"), actions: [
+        IconButton(
+          onPressed: _openAddExpenseOverlay,
+          icon: const Icon(Icons.add),
+        )
+      ]),
       body: Column(
         children: [
           const Text("Chart"),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses,)
-            ),
-
+              child: ExpensesList(
+            expenses: _registeredExpenses,
+          )),
         ],
       ),
     );
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
-class NewExpense extends StatefulWidget{
-  const NewExpense({super.key});
+class NewExpense extends StatefulWidget {
+  NewExpense(this.addNewExpense, {super.key});
+
+  Function addNewExpense; // TODO -- IMPLEMENT THIS METHOD!!
 
   @override
-  State<NewExpense> createState(){
+  State<NewExpense> createState() {
     return _NewExpenseState();
   }
 }
 
-class _NewExpenseState extends State<NewExpense>{
+class _NewExpenseState extends State<NewExpense> {
   var _enteredTitle = "";
 
   // catches changes made in the textfield below, and stores in a variable
@@ -19,11 +21,10 @@ class _NewExpenseState extends State<NewExpense>{
   DateTime? _selectedDate;
   Category _selectedCategory = Category.leisure;
 
-
   // tells flutter to shut down the TextEditingController when the overlay is closed
   // otherwise it lives on in memory
   @override
-  void dispose(){
+  void dispose() {
     _titleController.dispose();
     _amountController.dispose();
     super.dispose();
@@ -32,19 +33,46 @@ class _NewExpenseState extends State<NewExpense>{
   void _showDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, 1);
-     final pickedDate = await showDatePicker(
-      context: context, 
-      initialDate: now, 
-      firstDate: firstDate, 
-      lastDate: now
-      );
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
     setState(() {
       _selectedDate = pickedDate;
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null ||
+        enteredAmount <=
+            0; // returns false if no amount is given, or if it's zero or below
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      // no title, no or invalid amount, or no date returns an error
+      showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text("Invalid input!"),
+                content: const Text(
+                    "Please make sure valid Title, Date and Amount values are entered."),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(ctx);
+                      },
+                      child: Text("OK"))
+                ],
+              ));
+      return;
+    }
+    // after checks are made, everything is OK, we now add the input expense to the list
+  }
+
   @override
-  Widget build(context){
+  Widget build(context) {
     return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
@@ -56,63 +84,64 @@ class _NewExpenseState extends State<NewExpense>{
           ),
           Row(
             children: [
-            Expanded(
-              child: TextField(
-                controller: _amountController,
-                decoration: InputDecoration(
-                  prefixText: "\$",
-                  label: Text("Amount")
-                  ),
-                keyboardType: TextInputType.number,
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  decoration:
+                      InputDecoration(prefixText: "\$", label: Text("Amount")),
+                  keyboardType: TextInputType.number,
+                ),
               ),
-            ),
-          const SizedBox(width: 16,),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(_selectedDate == null ? "No Date Selected" : formatter.format(_selectedDate!)),
-                IconButton(onPressed: _showDatePicker, icon: const Icon(Icons.calendar_month)),
-              ],
-            ),
-            ),
-          ],
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(_selectedDate == null
+                        ? "No Date Selected"
+                        : formatter.format(_selectedDate!)),
+                    IconButton(
+                        onPressed: _showDatePicker,
+                        icon: const Icon(Icons.calendar_month)),
+                  ],
+                ),
+              ),
+            ],
           ),
-          SizedBox(height: 16,),
+          SizedBox(
+            height: 16,
+          ),
           Row(
             children: [
               DropdownButton(
-                value: _selectedCategory,
-                items: 
-                // gets the "values" enum list, call map() to convert each one into a dropdownmenuitem object, and then toList() on the whole thing
-                Category.values.map(
-                  (category) => DropdownMenuItem(
-                    value: category, 
-                    child: 
-                      Text(category.name.toUpperCase()))).toList(), 
-                    onChanged: (value) {
-                      if (value == null) {
-                          return;
-                      }
-                      setState(() {
-                        _selectedCategory = value;
-                      });
+                  value: _selectedCategory,
+                  items:
+                      // gets the "values" enum list, call map() to convert each one into a dropdownmenuitem object, and then toList() on the whole thing
+                      Category.values
+                          .map((category) => DropdownMenuItem(
+                              value: category,
+                              child: Text(category.name.toUpperCase())))
+                          .toList(),
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
                     }
-              ),
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
               const Spacer(),
               ElevatedButton(
-                onPressed: (){
-                  Navigator.pop(context);
-                }, 
-                child: Text("Cancel")
-              ),
+                  onPressed: () {
+                    Navigator.pop(
+                        context); // closes the overlay by 'popping' the context given to the current build method
+                  },
+                  child: Text("Cancel")),
+              const Spacer(),
               ElevatedButton(
-                onPressed: (){
-                  print(_titleController.text); // verify that the value is stored
-                  print(_amountController.text);
-                }, 
-                child: Text("Save Expense")
-              ),
+                  onPressed: _submitExpenseData, child: Text("Save Expense")),
             ],
           )
         ],
